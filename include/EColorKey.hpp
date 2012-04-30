8306ac3dd6dbf73d2492e37a85240c519e880b58
GUIHAL Maxime <sgp.jaconil@gmail.com>
2012-05-01 00:21:30 +0200

    Ecriture de EColorKey et EImage

---
+++ include/EColorKey.hpp
@@ -0,0 +1,25 @@
+#ifndef _ECOLORKEY
+#define _ECOLORKEY
+
+/*!
+ * \file EColorKey.hpp
+ * \brief Enumération des couleurs du skin
+ * \author Maxime GUIHAL
+ */
+
+namespace PolyBomber
+{ 
+	/*!
+	 * \enum EColorKey
+	 * \brief Codes des couleurs du skin
+	 */
+	enum EColorKey
+	{
+		TEXTCOLOR /*!< Couleur de texte normal */
+
+		// TODO: Remplir avec les autres libellés de couleur
+	};
+}
+
+#endif
+
---
+++ include/EImage.hpp
@@ -0,0 +1,26 @@
+#ifndef _EIMAGE
+#define _EIMAGE
+
+/*!
+ * \file EImage.hpp
+ * \brief Enumération des images du skin
+ * \author Maxime GUIHAL
+ */
+
+namespace PolyBomber
+{ 
+	/*!
+	 * \enum EImage
+	 * \brief Codes des images du skin
+	 */
+	enum EImage
+	{
+		LOGO /*!< Logo du programme */
+
+		// TODO: Remplir avec les autres libellés des images
+	};
+}
+
+#endif
+
+
