01d8bc60ad822781f5c2bb4c18c7c5b1d5e15e70
GUIHAL Maxime <sgp.jaconil@gmail.com>
2012-05-06 11:59:13 +0200

    Création du pattern Singleton

---
+++ include/TSingleton.hpp
@@ -0,0 +1,122 @@
+#ifndef _TSINGLETON
+#define _TSINGLETON
+
+/*!
+ * \file TSingleton.hpp
+ * \brief Patron de conception Singleton
+ * \author Maxime GUIHAL
+ */
+
+namespace PolyBomber
+{
+	/*!
+	 * \class Singleton<T>
+	 * \brief Patron de conception Singleton
+	 *
+	 * Ce patron de conception doit être utilisée par toutes les classes
+	 * de type *Manager de chaque composant, pour lesquelles <<singleton>>
+	 * est indiqué sur les diagrammes de classe, garantissant l'unicité de
+	 * cette classe pendant toute l'exécution.
+	 *
+	 * Exemple d'utilisation :
+	 *
+	 * == TestManager.hpp ==
+	 *
+	 * #include "TSingleton.hpp"
+	 *
+	 * class TestManager : public Singleton<TestManager>
+	 * {
+	 *     friend class Singleton<TestManager>;
+	 *
+	 *     private:
+	 *         string msg;
+	 *
+	 *         // Le constructeur doit être sans paramètres et privé
+	 *         TestManager()
+	 *         {
+	 *             this->msg = "test";
+	 *         }
+	 * 
+	 *     public:
+	 *         inline void setMessage(string m) {this->msg = m;}
+	 *         inline string getMessage() {return this->msg;}
+	 * };
+	 *
+	 * == ClasseUtilisantTestManager.cpp ==
+	 *
+	 * int main()
+	 * {
+	 *     TestManager* t = TestManager::getInstance();
+	 *
+	 *     t->setMessage("Maxime");
+	 *
+	 *     cout << t->getMessage() << endl;
+	 *
+	 *     // Penser à TOUJOURS faire un kill() après utilisation
+	 *     t->kill();
+	 *
+	 *     return 0;
+	 * }
+	 */
+	template<class T>
+	class Singleton
+	{
+		private:
+			static T* instance; /*!< Instance unique de l'objet */
+			static int nbInstances;	 /*!< Nombre d'instances de l'objet */
+
+		protected:
+			/*!
+			 * \brief Constructeur par défaut redéfinissable
+			 */
+			Singleton()	{}
+
+			/*!
+			 * \brief Destructeur par défaut redéfinissable
+			 */
+			virtual ~Singleton() {}
+
+		public:
+			/*!
+			 * \brief Méthode statique pour récupérer l'instance de l'objet
+			 * \return L'instance unique de l'objet
+			 */
+			static T* getInstance()
+			{
+				if (Singleton::instance == NULL)
+					Singleton::instance = new T;
+
+				Singleton::nbInstances++;
+								
+				return Singleton::instance;
+			}
+
+			/*!
+			 * \brief Méthode statique pour supprimer une instance de l'objet
+			 *
+			 * Cette méthode ne supprime l'objet que si plus aucun élément
+			 * ne la référence.
+			 */
+			static void kill()
+			{
+				if (Singleton::instance != NULL)
+				{
+					Singleton::nbInstances--;
+
+					if (Singleton::nbInstances == 0)
+					{
+						delete Singleton::instance;
+						Singleton::instance = NULL;
+					}
+				}				
+			}
+	};
+
+	/*!
+	 * \brief Initialisation des variables statiques
+	 */
+	template <class T> T* Singleton<T>::instance = NULL;
+	template <class T> int Singleton<T>::nbInstances = 0;
+}
+
+#endif
