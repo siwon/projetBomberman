504e66ef9d8b363ab353cea96cd4a5b582aa487e
GUIHAL Maxime <sgp.jaconil@gmail.com>
2012-05-08 15:03:03 +0200

    Ecriture de IGameInterfaceToMenu

---
+++ include/IGameInterfaceToMenu.hpp
@@ -0,0 +1,41 @@
+#ifndef _IGAMEINTERFACETOMENU
+#define _IGAMEINTERFACETOMENU
+
+/*!
+ * \file IGameInterfaceToMenu.hpp
+ * \brief Interface entre le composant Interface de jeu et Menu
+ * \author Maxime GUIHAL
+ */
+
+#include <SFML/RenderWindow.hpp>
+
+#include "EScreenSignal.hpp"
+
+namespace PolyBomber
+{
+	/*!
+	 * \class IGameInterfaceToMenu
+	 * \brief Interface Plateau de jeu - Menu
+	 */
+	class IGameInterfaceToMenu
+	{
+		public:
+			/*!
+			 * \brief Affichage du plateau de jeu
+			 *
+			 * Méthode appelée par le menu au début d'une partie pour
+			 * afficher le plateau de jeu
+			 *
+			 * \param window : Fenêtre du programme
+			 * \param score : Score des joueurs avant le début de la partie
+			 * \param winner : Référence vers le numéro du joueur qui gagnera
+			 * \return Signal de retour
+			 * 
+			 */
+			virtual EScreenSignal run(sf::RenderWindow window, unsigned int[4] score, int& winner) = 0;
+	};
+}
+
+#endif
+
+
