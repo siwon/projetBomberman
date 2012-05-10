4d9b619c056bd39021097e7fbce70508da264274
GUIHAL Maxime <sgp.jaconil@gmail.com>
2012-05-10 17:59:58 +0200

    Exception du jeu

---
+++ include/PolyBomberException.hpp
@@ -0,0 +1,31 @@
+#ifndef _POLYBOMBEREXCEPTION
+#define _POLYBOMBEREXCEPTION
+
+/*!
+ * \file PolyBomberException.hpp
+ * \brief Classe d'exception du programme
+ * \author Maxime GUIHAL
+ */
+
+namespace PolyBomber
+{
+	/*!
+	 * \class PolyBomberException
+	 * \brief Classe d'exception du programme
+	 */
+	class PolyBomberException : public std::exception
+	{
+		public:
+			PolyBomberException(const char* msg);
+
+			virtual ~PolyBomberException() throw();
+
+			virtual const char* what() const throw();
+
+		private:
+			std::string msg;
+	};
+}
+
+#endif
+
---
+++ src/PolyBomberException.cpp
@@ -0,0 +1,33 @@
+
+/*!
+ * \file PolyBomberException.cpp
+ * \brief Classe d'exception du programme
+ * \author Maxime GUIHAL
+ */
+
+#include <iostream>
+#include <sstream>
+#include <exception>
+
+#include "PolyBomberException.hpp"
+
+namespace PolyBomber
+{
+	PolyBomberException::PolyBomberException(const char* msg)
+	{
+		std::ostringstream oss;
+		oss << "Erreur : " << msg;
+		this->msg = oss.str();
+	}
+
+	PolyBomberException::~PolyBomberException() throw()
+	{}
+
+	const char* PolyBomberException::what() const throw()
+	{
+		return this->msg.c_str();
+	}
+}
+
+
+
