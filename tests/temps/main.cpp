840e87584e442f80bdd24721f1fd355089002cda
siwon <simon.rousseau1@etu.univ-nantes.fr>
2012-05-17 20:14:11 +0200

    ajout test temps

---
+++ tests/temps/main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/System/Time.hpp>
+#include <SFML/System/Clock.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	sf::Clock horloge = sf::Clock();
+	sf::Time tps = horloge.getElapsedTime();
+	int temps = horloge.getElapsedTime().asSeconds()+3;
+	
+	while (horloge.getElapsedTime().asSeconds()<temps) {
+		cout << "Waiting ..." << endl;
+	}
+	cout << "Boom" << endl;
+	
+    return 0;
+}
