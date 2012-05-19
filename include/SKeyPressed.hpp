65a5e73d1de457bcef9407750e00d66864dce031
siwon <simon.rousseau1@etu.univ-nantes.fr>
2012-05-19 22:28:13 +0200

    Update SKeyPressed.hpp comments

--- include/SKeyPressed.hpp
+++ include/SKeyPressed.hpp
@@ -17,6 +17,14 @@ namespace PolyBomber
 	 * du joueur (de 0 à 3), et la 2ème dimesion correspond à la touche
 	 * repérée par l'énumération EGameKeys. La matrice indique Vrai si
 	 * la touche est actuellement enfoncée, Faux sinon.
+	 * GAME_UP			< Aller vers le haut
+	 * GAME_DOWN		< Aller vers le bas
+	 * GAME_LEFT		< Aller vers la gauche
+	 * GAME_RIGHT		< Aller vers la droite
+	 * GAME_ACTION1		< Poser une bombe
+	 * GAME_ACTION2		< Activer le détonateur
+	 * GAME_PAUSE		< Mettre le jeu en pause
+	 * GAME_NONE		< Aucune touche pressée
 	 */
 	struct SKeyPressed
 	{
