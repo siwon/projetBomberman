055655288cebdfc91a8c37fa926b24f3a3ed5312
siwon <simon.rousseau1@etu.univ-nantes.fr>
2012-05-27 11:55:13 +0200

    ajout install

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Installation des librairies
+sudo apt-get update
+
+sudo apt-get install g++
+
+sudo apt-get install cmake
+sudo apt-get install libpthread-stubs0-dev
+sudo apt-get install libgl1-mesa-dev
+sudo apt-get install libx11-dev
+sudo apt-get install libxrandr-dev
+sudo apt-get install libfreetype6-dev
+sudo apt-get install libglew1.5-dev
+sudo apt-get install libjpeg-dev
+sudo apt-get install libsndfile1-dev
+sudo apt-get install libopenal-dev
+
+sudo apt-get install bluetooth
+sudo apt-get install libbluetooth-dev
+
+#Installation de WiiC
+cd lib/WiiC/
+sudo rm -rf build
+mkdir build
+cd build
+sudo cmake ../src
+sudo make
+cp wiic/libwiic.so ../../
+cp wiicpp/libwiicpp.so ../../
+cd ../../../
+
+
+#Installation de SFML2.0
+cd lib/SFML2.0/
+
+sudo cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=TRUE .
+sudo make
+sudo make install
+sudo cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug -D BUILD_SHARED_LIBS=TRUE .
+sudo make
+sudo make install
+sudo cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=FALSE .
+sudo make
+sudo make install
+sudo cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug -D BUILD_SHARED_LIBS=FALSE .
+sudo make
+sudo make install
