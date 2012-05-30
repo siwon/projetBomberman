aac9f61522404068bcd255d8a3e7fd82df2189f9
siwon <simon.rousseau1@etu.univ-nantes.fr>
2012-05-30 22:21:27 +0200

    .

--- include/EGameBonus.hpp
+++ include/EGameBonus.hpp
@@ -16,27 +16,25 @@ namespace PolyBomber
 	enum EGameBonus
 	{
 		SPEEDUP=0, /*!< Acceleration du joueur */
-		SPEEDDOWN, 
-		BOMBLINE, //bonus de bombe
-		DETONATOR,
-		BOMBUP,
-		BOMBDOWN,
-		RANGEUP,
-		RANGEDOWN,
-		RANGEUPMAX,
+		SPEEDDOWN, /*!< Décélération du joueur */
+		BOMBLINE, /*!< le joueur pose des lignes de bombes */
+		DETONATOR, /*!< Le joueur possède un détonateur */
+		BOMBUP, /*!< Augmentation du nombre de bombe pour le joueur */
+		BOMBDOWN, /*!< Diminution du nombre de bombe pour le joueur */
+		RANGEUP, /*!< Augmentation de la portée du joueur */
+		RANGEDOWN, /*!< Diminution de la portée du joueur */
+		RANGEUPMAX, /*!< Augmentation de la portée du joueur au maximum */
 		/*bonus de bombe*/
-		MINE,
-		INFINITYBOMB,
-		ATOMICBOMB,
+		MINE, /*!< mine pour le joueur */
+		INFINITYBOMB, /*!< bombe inifinie */
+		ATOMICBOMB, /*!< bombe atomique */
 		/*infection*/
-		CRANE,
-		HELL,
-		CONFUSION,
-		SPASME,
-		DILATATION,
-		RAGE
-		
-		//TODO : a commenter
+		CRANE, /*!< prend un infection au hasard */
+		HELL, /*!< donne une infection aléatoire à tous les joueurs */
+		CONFUSION, /*!< Touches du joueur inversée */
+		SPASME, /*!< Déplacement aléatoire du joueur toutes les X secondes */
+		DILATATION, /*!< Diminution au maximum de la vitesse du joueur */
+		RAGE /*!< Le joueur pose des bombes toutes les X secondes */
 	};
 }
 
