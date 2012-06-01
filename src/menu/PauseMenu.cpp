8938bba5108428fd930ce4c31d6c13d3d4c16e2d
GUIHAL Maxime <sgp.jaconil@gmail.com>
2012-06-01 18:03:04 +0200

    Pause

---
+++ src/menu/PauseMenu.cpp
@@ -0,0 +1,53 @@
+/*!
+ * \file PauseMenu.cpp
+ * \brief Gestionnaire du menu pause
+ * \author Maxime GUIHAL
+ */
+
+#include "menu/PauseMenu.hpp"
+#include "PolyBomberApp.hpp"
+
+#include "menu/ImageWidget.hpp"
+#include "menu/TextWidget.hpp"
+#include "menu/LinkWidget.hpp"
+
+namespace PolyBomber
+{
+	PauseMenu::PauseMenu() :
+		title("Pause", TITLEFONT, 100),
+		resume("Reprendre la partie", 250, RUNGAME),		
+		quit("Quitter la partie", 350, EXIT)
+	{
+		ISkin* skin = PolyBomberApp::getISkin();
+
+		title.setColor(skin->getColor(TITLECOLOR));
+
+		resume.setNext(&quit);
+		quit.setPrevious(&resume);
+
+		resume.setSelected(true);
+
+		this->widgets.push_back(&title);
+		this->widgets.push_back(&resume);
+		this->widgets.push_back(&quit);
+	}
+
+	void PauseMenu::downPressed()
+	{
+		resume.goNext();
+	}
+
+	void PauseMenu::upPressed()
+	{
+		quit.goPrevious();
+	}
+
+	void PauseMenu::validPressed(EMenuScreen* nextScreen)
+	{
+		if (resume.getSelected())
+			*nextScreen = resume.activate();
+			
+		if (quit.getSelected())
+			*nextScreen = quit.activate();
+	}
+}
